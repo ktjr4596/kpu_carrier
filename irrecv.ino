588b76a8dbe97de638ef359616f144c052f2c7f3
ktjr4596 <34061626+ktjr4596@users.noreply.github.com>
2018-01-23 15:51:14 +0900

    Update irrecv.ino

--- irrecv.ino
+++ irrecv.ino
@@ -94,4 +94,146 @@ void loop()
 
 }
 
+ //01.23
+#include <IRremote.h>
+
+#define motor_b_spd 3
+#define motor_b_dir1 4
+#define motor_b_dir2 5
+#define motor_a_dir1 7
+#define motor_a_dir2 8
+#define motor_a_spd 9
+
+
+//int trig=12;
+//int echo=13;
+
+void setmotor_a_go();
+void setmotor_b_go();
+
+//int RECV_PIN_LEFT = 5;
+
+int RECV_PIN_RIGHT=11;
+
+//IRrecv irrecv_left(RECV_PIN_LEFT);
+
+IRrecv irrecv_right(RECV_PIN_RIGHT);
+
+ 
+
+ 
+
+void setup()
+
+{
+
+   Serial.begin(9600);
+
+//  pinMode(RECV_PIN_LEFT,INPUT);
+
+  pinMode(RECV_PIN_RIGHT,INPUT);
+
   
+
+   pinMode(motor_a_dir1,OUTPUT);
+   pinMode(motor_a_dir2,OUTPUT);
+   pinMode(motor_b_dir1,OUTPUT);
+   pinMode(motor_b_dir2,OUTPUT);
+   
+   //pinMode(trig,OUTPUT);
+   //pinMode(echo,INPUT);
+   //irrecv_left.enableIRIn(); // Start the receiver
+
+  irrecv_right.enableIRIn();
+
+ 
+
+}
+
+ 
+
+void loop()
+
+{
+  
+  /*digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  
+  long duration=pulseIn(echo,HIGH);
+  long distance=(duration*340)/2/10000;
+  
+  Serial.print(distance);
+  Serial.println("cm");
+  delay(300);
+  */
+  int val_right=digitalRead(RECV_PIN_RIGHT);
+  
+
+    Serial.println("go");
+    setmotor_a_go();
+    setmotor_b_go();
+    analogWrite(motor_a_spd,200);
+    
+ 
+    analogWrite(motor_b_spd,200);
+    irrecv_right.resume();
+    delay(1000);
+  
+ /* else if(val_right==HIGH)
+  {
+    Serial.println("stop");
+    digitalWrite(motor_a_dir1,LOW);
+    digitalWrite(motor_a_dir2,LOW);
+    
+    digitalWrite(motor_b_dir1,LOW);
+    digitalWrite(motor_b_dir2,HIGH);
+    analogWrite(motor_b_spd,200);
+    irrecv_right.resume();
+    delay(1000);
+  }*/
+  
+  //int val_left=digitalRead(RECV_PIN_LEFT);
+
+ 
+
+  
+
+  
+
+  /*if(val_left==LOW)
+
+  {
+
+    digitalWrite(LEDLEFT,HIGH);
+
+    delay(100);
+
+    digitalWrite(LEDLEFT,LOW);
+
+    delay(100);
+
+  }*/
+
+
+  //irrecv_left.resume();
+
+  
+
+  delay(100);
+
+}
+
+
+void setmotor_a_go()
+{
+    digitalWrite(motor_a_dir1,HIGH);
+    digitalWrite(motor_a_dir2,LOW);
+}
+void setmotor_b_go()
+{
+  digitalWrite(motor_b_dir1,LOW);
+  digitalWrite(motor_b_dir2,HIGH);
+}
