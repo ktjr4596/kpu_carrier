2ea7752bf7dae4ecbe6264e21716723339f43201
ktjr4596 <34061626+ktjr4596@users.noreply.github.com>
2018-02-21 13:01:14 +0900

    Update irrecv.ino

--- irrecv.ino
+++ irrecv.ino
@@ -1,44 +1,48 @@
 #include <IRremote.h>
 
-#define LEDLEFT 3
-
-#define LEDRIGHT 4
 
  
+const int RECV_PIN_CENTER=11;
+const int RECV_PIN_LEFT =12;
+const int RECV_PIN_RIGHT=10;
 
-int RECV_PIN_LEFT = 5;
+//const int echo
 
-int RECV_PIN_RIGHT=6;
+static long dis=0;
+
+const int motorA_dir=8;
+const int motorA_spd=9;
+const int motorB_dir=4;
+const int motorB_spd=5;
 
 IRrecv irrecv_left(RECV_PIN_LEFT);
-
+IRrecv irrecv_center(RECV_PIN_CENTER);
 IRrecv irrecv_right(RECV_PIN_RIGHT);
 
+//long get_distance();
  
+void motor_go(const int &);
+void motor_left(const int &);
+void motor_left2(const int &);
+void motor_right(const int &);
+void motor_right2(const int &);
+void motor_stop();
 
  
 
 void setup()
 
 {
-
-  pinMode(LEDLEFT,OUTPUT);
-
-  pinMode(LEDRIGHT,OUTPUT);
-
-   Serial.begin(9600);
+  //pinMode(trig,OUTPUT);
+  //pinMode(echo,INPUT);
+  
+  Serial.begin(9600);
 
   pinMode(RECV_PIN_LEFT,INPUT);
 
   pinMode(RECV_PIN_RIGHT,INPUT);
 
-  
-
- 
-
-  irrecv_left.enableIRIn(); // Start the receiver
-
-  irrecv_right.enableIRIn();
+  pinMode(RECV_PIN_CENTER,INPUT);
 
  
 
@@ -49,49 +53,113 @@ void setup()
 void loop()
 
 {
-
+  
+  
   int val_left=digitalRead(RECV_PIN_LEFT);
 
  
 
   int val_right=digitalRead(RECV_PIN_RIGHT);
 
+  int val_center=digitalRead(RECV_PIN_CENTER); 
+  //dis=get_distance();
   
-
-  if(val_left==LOW)
+  if(!val_center&&val_right&&val_left)
+  {
+    //if(dis>=30)
+    //{
+      Serial.println("center");
+      motor_go(150);
+    delay(1000);
+    //}  
+}
+  
+  else if(!val_left&&!val_center)
 
   {
-
-    digitalWrite(LEDLEFT,HIGH);
-
-    delay(100);
-
-    digitalWrite(LEDLEFT,LOW);
-
-    delay(100);
-
+    motor_left2(150);
+    delay(1000);
   }
 
-  if(val_right==LOW)
+  else if(!val_left&&val_center)
 
   {
-
-    digitalWrite(LEDRIGHT,HIGH);
-
-    delay(100);
-
-    digitalWrite(LEDRIGHT,LOW);
-
-    delay(100);
-
+   motor_left(150);
+   delay(1000);
+  }else if(!val_right&&!val_center)
+  {
+    motor_right(150);
+    delay(1000);
   }
-
+  else if(!val_right&&val_center)
+  {
+    motor_right2(150);
+    delay(1000);
+  }
+  else{
+    //if(dir<30)
+    motor_stop();
+  }
   irrecv_left.resume();
 
   irrecv_right.resume();
-
+  irrecv_center.resume();
+  
   delay(100);
 
 }
 
   
+void motor_go(const int & spd)
+{
+  digitalWrite(motorA_dir,HIGH);
+  digitalWrite(motorB_dir,LOW);
+  analogWrite(motorA_spd,spd);
+  analogWrite(motorB_spd,spd);
+}
+
+void motor_left(const int & spd)
+{
+  analogWrite(motorA_spd,spd);
+  analogWrite(motorB_spd,spd-70);
+  
+}
+
+void motor_right(const int & spd)
+{
+  analogWrite(motorA_spd,spd-70);
+  analogWrite(motorB_spd,spd);
+}
+
+void motor_stop()
+{
+  analogWrite(motorA_spd,0);
+  analogWrite(motorB_spd,0);
+}
+
+void motor_left2(const int & spd)
+{
+  analogWrite(motorA_spd,spd);
+  analogWrite(motorB_spd,spd-100);
+}
+
+void motor_right2(const int & spd)
+{
+  analogWrite(motorB_spd,spd);
+  analogWrite(motorA_spd,spd-100);
+}
+
+/*double get_distance()
+{
+  digitalWrite(tirg,LOW);
+  delayMicroseconds(2);
+  digitalWrite(tirg,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  
+  long duration=pulseIn(echo,HIGH);
+  
+  long distance=(duration *340)/2/10000;
+  
+  return distance;
+}*/
